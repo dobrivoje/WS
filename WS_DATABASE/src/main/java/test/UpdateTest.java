efc708cacfb20e820eadb6e42c6cda5339b389dd
root <root@192.168.1.100>
2015-01-17 01:34:28 +0100

    WS Application Init. UpdateTest - small change.

--- WS_DATABASE/src/main/java/test/UpdateTest.java
+++ WS_DATABASE/src/main/java/test/UpdateTest.java
@@ -16,20 +16,35 @@ import org.superb.apps.ws.functionalities.ICustomer;
 public class UpdateTest {
 
     private static final ICustomer customerController = new CustomerController();
+    private static String errorMessage;
 
     public static void main(String[] args) {
-        Customer c2 = customerController.getCustomerByID(6L);
+        Customer c = customerController.getCustomerByID(6L);
 
-        c2.setName("Livnica Sevojno");
-        c2.setCity("Sevojno");
-        c2.setAddress("Vuka Karadžića bb");
-        c2.setZip("31410");
-        c2.setRegion("Zlatiborski okrug");
+        c.setName("Livnica Sevojno");
+        c.setCity("Sevojno");
+        c.setAddress("Vuka Karadžića bb");
+        c.setZip("31410");
+        c.setRegion("Zlatiborski okrug");
 
         try {
-            customerController.updateCustomer(c2);
-        } catch (Exception ex) {
-            System.err.println("Greška! Nije ažuriran postojeći kupac.");
+            customerController.updateCustomer(c);
+        } catch (Exception e) {
+            if (e.toString().contains("java.sql.SQLException: Cannot insert the value NULL into column")) {
+                errorMessage = "NULL Value detected! ";
+            }
+
+            if (e.toString().contains("Violation of UNIQUE KEY constraint \'CUSTOMER_PIB\'")) {
+                errorMessage += "PIB MUST BE UNIQUE ! ";
+            }
+
+            if (e.toString().contains("java.sql.SQLException: Violation of UNIQUE KEY constraint \'CUSTOMER_Unique_Data\'")) {
+                errorMessage += "CUSTOMER PROBABLY ALREADY EXIST !!!";
+            }
+        } finally {
+            if (!errorMessage.isEmpty()) {
+                System.err.println("Error: " + errorMessage);
+            }
         }
     }
 }
