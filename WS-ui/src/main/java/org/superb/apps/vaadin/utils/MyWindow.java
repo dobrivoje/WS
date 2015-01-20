d09e51b967499319fa22540d5befda95350136ce
dprtenjak <dprtenjak@D007012IML.intermol.sys.corp>
2015-01-20 19:15:28 +0100

    Izmene..

--- WS-ui/src/main/java/org/superb/apps/vaadin/utils/MyWindow.java
+++ WS-ui/src/main/java/org/superb/apps/vaadin/utils/MyWindow.java
@@ -8,6 +8,7 @@ package org.superb.apps.vaadin.utils;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Layout;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.VerticalSplitPanel;
 import com.vaadin.ui.Window;
@@ -50,6 +51,16 @@ public class MyWindow extends Window {
         setWidth(48, Unit.PERCENTAGE);
         center();
         setContent(vSP);
+    }
+
+    public MyWindow(Layout layout) {
+        super();
+
+        layout.setSizeFull();
+        setHeight(66, Unit.PERCENTAGE);
+        setWidth(48, Unit.PERCENTAGE);
+        center();
+        setContent(layout);
     }
 
     public void setText(String text) {
---
+++ WS-ui/src/main/java/org/superb/apps/ws/Forms/CustomerForm.java
@@ -0,0 +1,53 @@
+package org.superb.apps.ws.Forms;
+
+import com.vaadin.data.Item;
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.fieldgroup.PropertyId;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.TextField;
+import org.superb.apps.ws.db.entities.Customer;
+
+public class CustomerForm extends FormLayout {
+
+    private final FieldGroup fieldGroup = new BeanFieldGroup(Customer.class);
+    private Item bicCustomer;
+
+    //<editor-fold defaultstate="collapsed" desc="fields">
+    @PropertyId("Name")
+    private final TextField name = new TextField("customer Name");
+
+    @PropertyId("Address")
+    private final TextField address = new TextField("customer Address");
+
+    @PropertyId("City")
+    private final TextField city = new TextField("customer City");
+
+    @PropertyId("ZIP")
+    private final TextField zip = new TextField("customer ZIP");
+
+    @PropertyId("Region")
+    private final TextField region = new TextField("customer Region");
+
+    @PropertyId("PIB")
+    private final TextField pib = new TextField("customer PIB");
+    //</editor-fold>
+
+    public CustomerForm() {
+        setSizeFull();
+        setMargin(true);
+
+        fieldGroup.bindMemberFields(this);
+
+        addComponents(name, address, city, zip, region, pib);
+    }
+
+    public Item getBICCustomer() {
+        return bicCustomer;
+    }
+
+    public void setBICCustomer(Item BICCustomer) {
+        this.bicCustomer = BICCustomer;
+        fieldGroup.setItemDataSource(bicCustomer);
+    }
+}
